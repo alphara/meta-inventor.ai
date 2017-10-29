76a95c61b0dd925798e3def1cf398d92bac5182e
alpharak <artarakcheev@gmail.com>
2017-10-29 01:55:27 -0600

    Add more people and quantum computer and investment to dreams

--- dreams/src/Components/Dreams/Dreams.js
+++ dreams/src/Components/Dreams/Dreams.js
@@ -13,11 +13,18 @@ import Catamaran from './Images/Catamaran.jpg';
 import CatamaranLagoon from './Images/CatamaranLagoon.jpg';
 import Suite1 from './Images/Suite1.jpg';
 import Suite2 from './Images/Suite2.jpg';
-import FreddyMercury from './Images/FreddyMercury.jpg';
+import FreddieMercury from './Images/FreddieMercury.jpg';
 import RedhairGirl from './Images/RedhairGirl.png';
 import LatinaGirl from './Images/LatinaGirl.jpg';
 import NikolaTeslaFace from './Images/NikolaTeslaFace.jpg';
 import MariaOrsic from './Images/MariaOrsic.jpg';
+import MyBodyGraph from './Images/MyBodyGraph.png';
+import MiltonErickson from './Images/MiltonErickson.jpg';
+import Osho from './Images/Osho.jpg';
+import SteveJobs from './Images/SteveJobs.jpg';
+import Neuron from './Images/Neuron.jpg';
+import QuantumComputer from './Images/QuantumComputer.jpg';
+import Investment from './Images/Investment.jpg';
 
 class Dreams extends Component {
   render() {
@@ -36,11 +43,18 @@ class Dreams extends Component {
         <img src={CatamaranLagoon} className="Dreams__CatamaranLagoon" alt="Catamaran Lagoon" />
         <img src={Suite1} className="Dreams__Suite1" alt="Suite 1" />
         <img src={Suite2} className="Dreams__Suite2" alt="Suite 2" />
-        <img src={FreddyMercury} className="Dreams__FreddyMercury" alt="Freddy Mercury" />
+        <img src={FreddieMercury} className="Dreams__FreddieMercury" alt="Freddie Mercury" />
         <img src={RedhairGirl} className="Dreams__RedhairGirl" alt="Redhair Girl" />
         <img src={LatinaGirl} className="Dreams__LatinaGirl" alt="Latina Girl" />
         <img src={NikolaTeslaFace} className="Dreams__NikolaTeslaFace" alt="Nikola Tesla Face" />
         <img src={MariaOrsic} className="Dreams__MariaOrsic" alt="Maria Orsic" />
+        <img src={MyBodyGraph} className="Dreams__MyBodyGraph" alt="My Body Graph" />
+        <img src={MiltonErickson} className="Dreams__MiltonErikson" alt="Milton Erikson" />
+        <img src={Osho} className="Dreams__Osho" alt="Osho" />
+        <img src={SteveJobs} className="Dreams__SteveJobs" alt="Steve Jobs" />
+        <img src={Neuron} className="Dreams__Neuron" alt="Neuron" />
+        <img src={QuantumComputer} className="Dreams__QuantumComputer" alt="Quantum Computer" />
+        <img src={Investment} className="Dreams__Investment" alt="Investment" />
       </div>
     );
   }
